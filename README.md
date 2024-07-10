d7843f6604c9455f88ced97dd8964b73aa1d1e59
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2024-07-10 09:25:33 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Flake test
+
+I want to create a flake that can provide me with multiple shells
+
+## How to run
+
+### Develop in serviceA
+```shell
+nix develop .#serviceA
+```
+
+the shell comes with fish shell, but it is not loaded automatically
+to use fish shell, run this command
+```shell
+nix develop .#serviceA --command fish
+```
+
+### Develop in serviceB
+```shell
+nix develop .#serviceB
+```
+
+the shell comes with fish shell, but it is not loaded automatically
+to use fish shell, run this command
+```shell
+nix develop .#serviceB --command fish
+```
---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let 
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+
+    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    devShells.x86_64-linux.serviceA = pkgs.mkShell {
+      buildInputs = [ pkgs.nodejs pkgs.fish ];
+      shellHook = ''
+        cd serviceA
+      '';
+    };
+
+    devShells.x86_64-linux.serviceB = pkgs.mkShell {
+      buildInputs = [ pkgs.go pkgs.fish ];
+
+      shellHook = ''
+        cd serviceB
+      '';
+    };
+
+  };
+}
